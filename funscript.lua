4f504daf5d5845faef6718684d58cfccd5839833
whoppper11 <136935003+whoppper11@users.noreply.github.com>
2025-02-19 16:45:07 +0000

    Update funscript.lua

--- funscript.lua
+++ funscript.lua
@@ -1,4 +1,4 @@
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
-player:Kick("Hi erm thsi is the creator of the script i have your IP and you on my dox lol")
+player:Kick("Hi erm this is the creator of the script i have your IP and you on my dox lol")
